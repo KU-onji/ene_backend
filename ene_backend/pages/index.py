4adabae8dd8764d1d4116589288443f8db16ecd1
KU-onji <mgr_127@yahoo.co.jp>
2024-06-02 19:17:12 +0900

    sample reflex

---
+++ ene_backend/pages/index.py
@@ -0,0 +1,18 @@
+"""The home page of the app."""
+
+import reflex as rx
+
+from ene_backend import styles
+from ene_backend.templates import template
+
+
+@template(route="/", title="Home")
+def index() -> rx.Component:
+    """The home page.
+
+    Returns:
+        The UI for the home page.
+    """
+    with open("README.md", encoding="utf-8") as readme:
+        content = readme.read()
+    return rx.markdown(content, component_map=styles.markdown_style)
