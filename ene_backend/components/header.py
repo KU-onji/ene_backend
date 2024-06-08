2fd3170641e9ead190e57574f29acdb4c42edc0b
KU-onji <mgr_127@yahoo.co.jp>
2024-06-08 20:32:16 +0900

    fix bug on production environment

--- ene_backend/components/header.py
+++ ene_backend/components/header.py
@@ -78,7 +78,7 @@ def navi_bar() -> rx.Component:
                 rx.button(
                     rx.icon(tag="settings"),
                     variant="solid",
-                    size="4",
+                    size="md",
                     on_click=rx.redirect("/user_profile"),
                     color_scheme="iris",
                     border_radius=styles.border_radius,
